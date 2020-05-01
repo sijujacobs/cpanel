44c0c48e0277c814bd8a1e46320cc0a1e4a41e9c
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-05-01 14:23:19 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+(function () {
+  console.log("------C-Panel:: index.js-------------");
+
+  //   echo "# cpanel" >> README.md
+  // git init
+  // git add README.md
+  // git commit -m "first commit"
+  // git remote add origin https://github.com/sijujacobs/cpanel.git
+  // git push -u origin master
+})();
